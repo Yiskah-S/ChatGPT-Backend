61795ae07369ef30495c25783bd36cac773640c9
Jessica <Yiskah.Scientia@gmail.com>
2023-08-13 20:47:19 -0400

    Added additional logging for debugging

--- app/routes/api_key_routes.py
+++ app/routes/api_key_routes.py
@@ -1,53 +1,53 @@
-# api_key_routes.py 
-
+import logging
 from flask_login import current_user, login_required
 from flask import Blueprint, jsonify, request
 from app import db
 from app.models.api_key import APIKey
 from app.models.user import User
 
+logging.basicConfig(level=logging.DEBUG)
+
 api_key_bp = Blueprint('api_key', __name__, url_prefix='/api-keys/<int:user_id>/')
 
 @login_required
 @api_key_bp.route('/', methods=['GET'])
 def get_api_keys(user_id):
-    # debug
-    print(f"Current user id: {current_user.get_id()}")
-
+    logging.debug(f"Current user id: {current_user.get_id()}")
+    # print(f"Current user id: {current_user.get_id()}")
     api_keys = APIKey.query.filter_by(user_id=user_id).all()
 
     if not api_keys:
         return jsonify({"error": "No API keys found for this user."}), 404
 
     api_keys_data = [api_key.to_dict() for api_key in api_keys]
-    # debug
-    print(f"API keys data: {api_keys_data}")
+    logging.debug(f"API keys data: {api_keys_data}")
+    # print(f"API keys data: {api_keys_data}")
     return jsonify(api_keys_data), 200
 
 @login_required
 @api_key_bp.route('/', methods=['PUT'])
 def update_api_key(user_id):
-	data = request.get_json()
-	# debug
-	print("Received data:", data)
-	print(f"Current user id: {current_user.get_id()}")
+    data = request.get_json()
+    logging.debug("Received data: " + str(data))
+    logging.debug(f"Current user id: {current_user.get_id()}")
+    # print("Received data:", data)
+    # print(f"Current user id: {current_user.get_id()}")
+    api_keys = data.get('apiKeys')
 
-	api_keys = data.get('apiKeys')
+    if not api_keys:
+        return jsonify({"error": "No API keys provided."}), 400
 
-	if not api_keys:
-		return jsonify({"error": "No API keys provided."}), 400
+    for key_obj in api_keys:
+        key = APIKey.query.filter_by(user_id=user_id, api_type=key_obj['api_type']).first()
+        if key:
+            key.api_key = key_obj['api_key']
+        else:
+            new_api_key = APIKey(api_type=key_obj['api_type'], api_key=key_obj['api_key'], user_id=user_id)
+            db.session.add(new_api_key)
 
-	for key_obj in api_keys:
-		key = APIKey.query.filter_by(user_id=user_id, api_type=key_obj['api_type']).first()
-		if key:
-			key.api_key = key_obj['api_key']
-		else:
-			new_api_key = APIKey(api_type=key_obj['api_type'], api_key=key_obj['api_key'], user_id=user_id)
-			db.session.add(new_api_key)
+    db.session.commit()
 
-	db.session.commit()
-
-	return jsonify({"message": "API keys updated successfully."}), 200
+    return jsonify({"message": "API keys updated successfully."}), 200
 
 @login_required
 @api_key_bp.route('/<int:api_key_id>', methods=['DELETE'])
--- app/routes/prompt_routes.py
+++ app/routes/prompt_routes.py
@@ -1,83 +1,62 @@
-# prompt_routes.py
-
+import logging
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user, login_user, logout_user
 from app import db
 from app.models.prompt import Prompt
+
+logging.basicConfig(level=logging.DEBUG)
 
 prompt_bp = Blueprint('prompt', __name__, url_prefix='/prompts/<int:user_id>/')
 
 @login_required
 @prompt_bp.route('/', methods=['POST'])
 def create_prompt(user_id):
-	print(f"Current user id: {current_user.get_id()}")
-	print(f"User id from URL: {user_id}")
-	print(f"Request body: {request.get_json()}")
+    logging.debug(f"Current user id: {current_user.get_id()}")
+    logging.debug(f"User id from URL: {user_id}")
+    logging.debug(f"Request body: {request.get_json()}")
+    # print(f"Current user id: {current_user.get_id()}")
+    # print(f"User id from URL: {user_id}")
+    # print(f"Request body: {request.get_json()}")
 
-	data = request.get_json()
-	new_prompt = Prompt.from_dict(data, user_id=user_id)
-	db.session.add(new_prompt)
-	db.session.commit()
-	return jsonify(new_prompt.to_dict()), 201
+    data = request.get_json()
+    new_prompt = Prompt.from_dict(data, user_id=user_id)
+    db.session.add(new_prompt)
+    db.session.commit()
+    return jsonify(new_prompt.to_dict()), 201
 
 @login_required
 @prompt_bp.route('/', methods=['GET'])
 def get_prompts(user_id):
-	prompts = Prompt.query.filter_by(user_id=user_id).all()
-	
-	if not prompts:
-		return jsonify({"error": "No prompts found for this user."}), 404
-	
-	prompts_data = [prompt.to_dict() for prompt in prompts]
-	return jsonify(prompts_data), 200
+    prompts = Prompt.query.filter_by(user_id=user_id).all()
 
-@login_required
-@prompt_bp.route('/<int:prompt_id>', methods=['GET'])
-def get_prompt(user_id, prompt_id):
-	prompt = Prompt.query.filter_by(user_id=user_id, id=prompt_id).first()
-	if not prompt:
-		return jsonify({"error": "Prompt not found."}), 404
-	return jsonify(prompt.to_dict()), 200
+    if not prompts:
+        return jsonify({"error": "No prompts found for this user."}), 404
+
+    prompts_data = [prompt.to_dict() for prompt in prompts]
+    return jsonify(prompts_data), 200
 
 @login_required
 @prompt_bp.route('/<int:prompt_id>', methods=['PUT'])
 def update_prompt(user_id, prompt_id):
-	prompt = Prompt.query.filter_by(user_id=user_id, id=prompt_id).first()
-	if not prompt:
-		return jsonify({"error": "Prompt not found."}), 404
-	data = request.get_json()
-	prompt.update(data)
-	db.session.commit()
-	return jsonify(prompt.to_dict()), 200
+    data = request.get_json()
+    logging.debug("Received data: " + str(data))
+    # print("Received data:", data)
+    prompt = Prompt.query.filter_by(user_id=user_id, id=prompt_id).first()
+
+    if not prompt:
+        return jsonify({"error": "Prompt not found."}), 404
+
+    prompt.update_from_dict(data)
+    db.session.commit()
+
+    return jsonify(prompt.to_dict()), 200
 
 @login_required
 @prompt_bp.route('/<int:prompt_id>', methods=['DELETE'])
 def delete_prompt(user_id, prompt_id):
-	prompt = Prompt.query.filter_by(user_id=user_id, id=prompt_id).first()
-	if not prompt:
-		return jsonify({"error": "Prompt not found."}), 404
-	db.session.delete(prompt)
-	db.session.commit()
-	return jsonify({"message": "Prompt successfully deleted."}), 200
-
-@login_required
-@prompt_bp.route('/categories', methods=['GET'])
-def get_prompt_categories(user_id):
-	prompts = Prompt.query.filter_by(user_id=user_id).all()
-	
-	if not prompts:
-		return jsonify({"error": "No prompts found for this user."}), 404
-	
-	categories = {prompt.category for prompt in prompts}
-	return jsonify(list(categories)), 200
-
-@login_required
-@prompt_bp.route('/categories/<string:category>', methods=['GET'])
-def get_prompts_by_category(user_id, category):
-	prompts = Prompt.query.filter_by(user_id=user_id, category=category).all()
-	
-	if not prompts:
-		return jsonify({"error": "No prompts found for this user."}), 404
-	
-	prompts_data = [prompt.to_dict() for prompt in prompts]
-	return jsonify(prompts_data), 200
+    prompt = Prompt.query.get(prompt_id)
+    if not prompt:
+        return jsonify({"error": "Prompt not found."}), 404
+    db.session.delete(prompt)
+    db.session.commit()
+    return jsonify({"message": "Prompt successfully deleted."}), 200
